5c674e2a254ba7887e7ef7e44d76b756684584cf
PmcFizz <mengchen_0212@foxmail.com>
2019-05-06 22:02:59 +0800

    准备下班了

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('sass', function () {
 gulp.task('default', function () {
   browserSync.init({
     server: {
-      baseDir: './view/'
+      baseDir: './'
     },
     port: 8050
   })
